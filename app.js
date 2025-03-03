b452d8644d7d11e61c396f16dec725dd9b60bdb9
Prakash Parmar <prakash07dz@gmail.com>
2025-03-03 07:36:14 +0530

    updated app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
-const MongoStore = require("connect-mongo");
+const MongoStore = require('connect-mongo');
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
@@ -18,18 +18,15 @@ const User = require("./models/user.js");
 const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
-const cors = require("cors");
+const { x } = require("joi");
 
 const dbUrl = process.env.ATLASDB_URL;
 
-// MongoDB connection
-main()
-    .then(() => {
-        console.log("Connected to DB");
-    })
-    .catch((err) => {
-        console.log("DB Connection Error:", err);
-    });
+main().then(() => {
+    console.log("connected to DB");
+}).catch((err) => {
+    console.log(err);
+});
 
 async function main() {
     await mongoose.connect(dbUrl);
@@ -41,7 +38,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
-app.use(cors());
 
 const store = MongoStore.create({
     mongoUrl: dbUrl,
@@ -51,8 +47,8 @@ const store = MongoStore.create({
     touchAfter: 24 * 3600,
 });
 
-store.on("error", (err) => {
-    console.log("Error in Mongo Session Store:", err);
+store.on("error", () => {
+    console.log("ERROR IN MONGO SESSION STORE", err);
 });
 
 const sessionOptions = {
@@ -64,21 +60,18 @@ const sessionOptions = {
         expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
-    },
+    }
 };
 
 app.use(session(sessionOptions));
 app.use(flash());
-
 app.use(passport.initialize());
 app.use(passport.session());
+
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-// Flash messages and current user
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
@@ -86,29 +79,20 @@ app.use((req, res, next) => {
     next();
 });
 
-// Routes
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
-// Home route
-app.get("/", (req, res) => {
-    res.redirect("/listings");
-});
-
-// 404 Handler
 app.all("*", (req, res, next) => {
     next(new ExpressError(404, "Page Not Found!"));
 });
 
-// Error Handler
 app.use((err, req, res, next) => {
     let { statusCode = 500, message = "Something went wrong" } = err;
+    // res.status(statusCode).send(message);
     res.status(statusCode).render("listings/error.ejs", { message });
 });
 
-// Dynamic port for deployment
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+app.listen(8080, () => {
+    console.log("server is listning at port 8080");
 });
